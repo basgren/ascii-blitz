07ef806a29eea0d2587b3ffb978821cf9f580bbd
Basil Gren <bas.gren@gmail.com>
2025-07-13 17:54:40 +0200

    Refactor keyboard input

--- AsciiBlitz/Core/Game.cs
+++ AsciiBlitz/Core/Game.cs
@@ -1,4 +1,5 @@
-﻿using AsciiBlitz.Core.Map;
+﻿using AsciiBlitz.Core.Input;
+using AsciiBlitz.Core.Map;
 using AsciiBlitz.Core.Map.Generator;
 using AsciiBlitz.Core.Map.Objects;
 using AsciiBlitz.Core.Render;
@@ -7,17 +8,20 @@ using AsciiBlitz.Core.Types;
 namespace AsciiBlitz.Core;
 
 public class Game {
-
   private GameMap _map;
-  
+  private IGameInput _input = new ConsoleInput();
+
+  private bool _gameRunning = true;
+
   public void Run() {
     Console.Clear();
     Console.CursorVisible = false;
     Console.WriteLine("ASCII Blitz Game");
+    InitInput();
 
 
     IMapGenerator mapGen = new TestMapGenerator();
-    
+
     _map = mapGen
       .SetSize(30, 10)
       .Build();
@@ -25,87 +29,72 @@ public class Game {
     // Test rendering - when needed to show generated map.
     // GameMapRenderer mapRenderer = new GameMapRenderer();
     // mapRenderer.Render(map);
-    
+
     MapGridRenderer mapRenderer = new();
-    
+
     // Main game loop - 60 FPS
     const int targetFps = 30;
     const int frameTimeMs = 1000 / targetFps;
-    
-    bool gameRunning = true;
+
     var startTime = DateTime.Now;
     var lastFrameTime = DateTime.Now;
-    
-    while (gameRunning) {
+
+    while (_gameRunning) {
       var currentTime = DateTime.Now;
       var deltaTime = (currentTime - lastFrameTime).TotalMilliseconds;
       var totalTimeSeconds = (currentTime - startTime).TotalSeconds;
-      
-      // Handle input
-      ConsoleKeyInfo keyInfo = default;
-      bool keyPressed = false;
-      
-      if (Console.KeyAvailable) {
-        keyInfo = Console.ReadKey(true);
-        keyPressed = true;
-        
-        // Check for ESC to exit
-        if (keyInfo.Key == ConsoleKey.Escape) {
-          gameRunning = false;
-          continue;
-        }
-      }
-      
-      mapRenderer.Render(_map, totalTimeSeconds);
-      
-      // Display key information at position 0,0
-      Console.SetCursorPosition(0, 0);
-      
-      if (keyPressed) {
-        // Display keycode for captured keys
-        switch (keyInfo.Key) {
-          case ConsoleKey.UpArrow:
-            TryMove(Direction.Up);
-            break;
-          
-          case ConsoleKey.DownArrow:
-            TryMove(Direction.Down);
-            break;
-          
-          case ConsoleKey.LeftArrow:
-            TryMove(Direction.Left);
-            break;
 
-          case ConsoleKey.RightArrow:
-            TryMove(Direction.Right);
-            break;
-          
-          case ConsoleKey.Spacebar:
-          case ConsoleKey.C:
-            // Console.Write($"Key: {(int)keyInfo.Key}    ");
-            break;
-          default:
-            // Console.Write("Key: 0   ");
-            break;
-        }
-      } else {
-        // No key pressed
-        // Console.Write("Key: 0   ");
-      }
-      
+      _input.Update();
+
+      mapRenderer.Render(_map, totalTimeSeconds);
+
       // Frame rate limiting
       if (deltaTime < frameTimeMs) {
         Thread.Sleep((int)(frameTimeMs - deltaTime));
       }
-      
+
       lastFrameTime = DateTime.Now;
     }
   }
 
+  private void InitInput() {
+    _input.MoveLeft += OnMoveLeft;
+    _input.MoveRight += OnMoveRight;
+    _input.MoveUp += OnMoveUp;
+    _input.MoveDown += OnMoveDown;
+    _input.Fire += OnFire;
+    _input.Quit += OnQuit;
+  }
+
+  private void OnMoveLeft() {
+    TryMove(Direction.Left);
+  }
+
+  private void OnMoveRight() {
+    TryMove(Direction.Right);
+  }
+
+  private void OnMoveUp() {
+    TryMove(Direction.Up);
+  }
+
+  private void OnMoveDown() {
+    TryMove(Direction.Down);
+  }
+  
+  private void OnFire() {
+    // do nothing for now
+  }
+
+  private void OnQuit() {
+    _gameRunning = false;
+  }
+  
+
   private void TryMove(Direction dir) {
     var layer = _map.GetUnitLayer();
     Vec2Int offs = new Vec2Int(0, 0);
-    
+
     switch (dir) {
       case Direction.Up:
         offs = Vec2Int.Up;
@@ -114,22 +103,22 @@ public class Game {
       case Direction.Down:
         offs = Vec2Int.Down;
         break;
-      
+
       case Direction.Left:
         offs = Vec2Int.Left;
         break;
-      
+
       case Direction.Right:
         offs = Vec2Int.Right;
         break;
     }
-    
+
     var player = layer.Player;
     player.Dir = dir;
 
     if (_map.CanMove(player.Pos, offs)) {
       player.Pos += offs;
-      
+
       MapObject? obj = _map.GetLayer(0).GetAt(player.Pos.X, player.Pos.Y);
 
       if (obj != null) {
---
+++ AsciiBlitz/Core/Input/ConsoleInput.cs
@@ -0,0 +1,54 @@
+﻿namespace AsciiBlitz.Core.Input;
+
+public class ConsoleInput : IGameInput {
+  public event Action? MoveLeft;
+  public event Action? MoveRight;
+  public event Action? MoveUp;
+  public event Action? MoveDown;
+  public event Action? Fire;
+  public event Action? Quit;
+
+  public void Update() {
+    ConsoleKeyInfo? keyInfo = null;
+
+    if (Console.KeyAvailable) {
+      keyInfo = Console.ReadKey(true);
+      
+      // Also exhaust buffer, as it we hold key, it will accumulate keypresses, and consequent calls of
+      // ReadKey will give us the same key for some time even if we've already released the button.
+      while (Console.KeyAvailable) {
+        Console.ReadKey(true);
+      }
+    }
+
+    if (!keyInfo.HasValue) {
+      return;
+    }
+
+    switch (keyInfo.Value.Key) {
+      case ConsoleKey.Escape:
+        Quit?.Invoke();
+        break;
+
+      case ConsoleKey.UpArrow:
+        MoveUp?.Invoke();
+        break;
+
+      case ConsoleKey.DownArrow:
+        MoveDown?.Invoke();
+        break;
+
+      case ConsoleKey.LeftArrow:
+        MoveLeft?.Invoke();
+        break;
+
+      case ConsoleKey.RightArrow:
+        MoveRight?.Invoke();
+        break;
+
+      case ConsoleKey.Spacebar:
+        Fire?.Invoke();
+        break;
+    }
+  }
+}
